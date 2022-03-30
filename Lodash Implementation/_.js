078ccd82b1b7bcc4e569023b337cc7670ff0d179
Carlos <carlos.alvarezdelavega@gmail.com>
2022-03-30 18:47:11 +0100

    add clamp() method

--- Lodash Implementation/_.js
+++ Lodash Implementation/_.js
@@ -1,9 +1,17 @@
 // The Lodash Object
 const _ = {
-
+    clamp(num, lowerBound, upperBound) {
+        if (num <= lowerBound) {
+            return lowerBound;
+        } else if(num >= upperBound) {
+            return upperBound;
+        } else {
+            return num;
+        }
+    }, // End clamp()
 }; // End Lodash Object
 
-
+console.log(_.clamp(5,6,8))
 
 
 // Do not write or modify code below this line.
